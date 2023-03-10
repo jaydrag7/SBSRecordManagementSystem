b571dfda1f8ba091405fe5ebe5365ab3976e1397
jaydrag7 <118785492+jaydrag7@users.noreply.github.com>
2023-03-10 02:59:28 -0500

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,25 @@
+package src.grade;
+import java.util.*;
+public class Grade{
+
+    private  HashMap<String, Integer> grades= new HashMap<>();
+
+
+    public Grade(HashMap<String, Integer> grades){
+        this.grades=grades; 
+    }
+
+
+    public int getGrade(String subject){
+        return this.grades.get(subject);
+    }
+
+    public void setGrade(String subject,int mark){
+        this.grades.replace(subject,mark);
+    }
+
+    public HashMap<String, Integer> getGrades(){
+        return grades;
+    }
+
+}
